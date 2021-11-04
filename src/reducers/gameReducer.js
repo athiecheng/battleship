07e34b67efe45c938bbe6639ccfbeaeba04b1bc6
belicephia <belicephia@gmail.com>
2021-11-03 20:51:41 -0700

    update

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -25,21 +25,38 @@ function generateGameBoard() {
     console.log(defaultState);
     
     for (let i = 2; i < 6; i++) {
-        let dir = getRandomInt(2);
-        let startx = getRandomInt(5);
-        let starty = getRandomInt(5);
-        for (let j = 0; j < i; j++) {
-            if (dir === 0) {
-                console.log("hori"+ i + j);
-                defaultState.boxMap[startx][starty] = "X";
-                startx +=1;
-            }
-            else{
-                console.log("hori"+ i + j)
-                defaultState.boxMap[startx][starty] = "X";
-                starty +=1;
-            }
+        let allclear = false;
+        while (allclear == false){
+            let dir = getRandomInt(2);
+            let startx = getRandomInt(5);
+            let starty = getRandomInt(5);
+            let count = 0;
+            while (defaultState.boxMap[startx][starty] != "X" {
+                if (dir === 0) {
+                    startx +=1;
+                }
+                else{
+                    starty +=1;
+                }
+                count += 1;
+                if (count == i){
+                    for (let j = 0;j<i;j++){
+                        if (dir === 0){
+                            defaultState.boxMap[startx][starty] = "X"
+                            startx -=1
+                        }
+                        else{
+                            defaultState.boxMap[startx][starty] = "X"
+                            starty -=1
+                        }
+                        
+                    }
+                    allclear = true;
+                }
+                
 
+        }
+        
 
         }
         console.log("ship of "+ i)
