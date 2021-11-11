5406cab0e8534e643287a5664a656b2babf0416f
athiecheng <72422874+athiecheng@users.noreply.github.com>
2021-11-11 11:56:56 -0800

    combining reducer

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -11,7 +11,19 @@ const defaultState = {
         ['', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '']
-    ]
+    ],
+    robotMap: [
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', '']
+    ],
 
 };
 
@@ -32,7 +44,7 @@ function generateGameBoard() {
             let startx = getRandomInt(10);
             let starty = getRandomInt(10);
             let count = 0;
-            while (startx<=9 && starty<=9 && newstate.boxMap[startx][starty] != "X"   ){
+            while (startx<=9 && starty<=9 && newstate.boxMap[startx][starty] != "X" ){
                 if (dir === 0) {
                     startx +=1;
                 }
@@ -75,6 +87,57 @@ function generateGameBoard() {
         
 
     }
+
+    for (let i = 2; i < 6; i++) {
+        let allclear = false;
+        while (allclear == false){
+            
+            let dir = getRandomInt(2);
+            let startx = getRandomInt(10);
+            let starty = getRandomInt(10);
+            let count = 0;
+            while (startx<=9 && starty<=9 && newstate.robotMap[startx][starty] != "X" ){
+                if (dir === 0) {
+                    startx +=1;
+                }
+                else{
+                    starty +=1;
+                }
+                count += 1;
+                if (count == i){
+                    for (let j = 0;j<i;j++){
+                        if (dir === 0){
+                            startx -=1
+                            console.log(startx+" row "+starty);
+                            newstate.robotMap[startx][starty] = "X"
+                            
+                        }
+                        else{
+                            starty -=1
+                            console.log(startx+" col "+starty);
+                            newstate.robotMap[startx][starty] = "X"
+                            
+                        }
+                    }
+                    allclear = true;
+                    if (dir === 0){
+                        startx +=1
+                    }
+                    else{
+                        starty +=1
+                    }
+                    
+                }
+                
+
+        }
+        
+
+        }
+        console.log("ship of "+ i)
+        
+
+    }
     return newstate;
 }
 
@@ -85,13 +148,14 @@ export default function gameReducer(state, action) {
     }
 
     if (action.type === 'playGame'){
-        for (var i = 0; i < state.boxMap.length; i++) {
-            for( var j = 0; j < state.boxMap[0].length; j++){
-                state.boxMap[i][j] = ''
-            }
-        }
+
         return generateGameBoard()
     }
+
+    // if(action.type === 'normalPlay') {
+    //     generateGameBoard(boxMap)
+    //     return generateGameBoard(robotMap)
+    // }
 
     if (action.type === 'boardClick') {
         const value = state.boxMap[action.x][action.y];
--- src/reducers/robotReducer.js
+++ src/reducers/robotReducer.js
@@ -1,119 +1,130 @@
-const defaultState = {
-    blackBox: 0,
-    boxMap: [
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '']
-    ]
+// const defaultState = {
+//     blackBox: 0,
+//     boxMap: [
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', ''],
+//         ['', '', '', '', '', '', '', '', '', '']
+//     ]
 
-};
+// };
 
-function getRandomInt(max) {
-    // min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max + 1));
-}
+// function getRandomInt(max) {
+//     // min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max + 1));
+// }
 
-function generateGameBoard() {
-    const newstate = defaultState
-    console.log("start" +newstate);
-    for (let i = 2; i < 6; i++) {
-        let allclear = false;
-        while (allclear == false){
+// function generateGameBoard() {
+//     const newstate = defaultState
+//     console.log("start" +newstate);
+//     for (let i = 2; i < 6; i++) {
+//         let allclear = false;
+//         while (allclear == false){
             
-            let dir = getRandomInt(2);
-            let startx = getRandomInt(10);
-            let starty = getRandomInt(10);
-            let count = 0;
-            while (startx<=9 && starty<=9 && newstate.boxMap[startx][starty] != "X"   ){
-                if (dir === 0) {
-                    startx +=1;
-                }
-                else{
-                    starty +=1;
-                }
-                count += 1;
-                if (count == i){
-                    for (let j = 0;j<i;j++){
-                        if (dir === 0){
-                            startx -=1
-                            console.log(startx+" row "+starty);
-                            newstate.boxMap[startx][starty] = "X"
+//             let dir = getRandomInt(2);
+//             let startx = getRandomInt(10);
+//             let starty = getRandomInt(10);
+//             let count = 0;
+//             while (startx<=9 && starty<=9 && newstate.boxMap[startx][starty] != "X"   ){
+//                 if (dir === 0) {
+//                     startx +=1;
+//                 }
+//                 else{
+//                     starty +=1;
+//                 }
+//                 count += 1;
+//                 if (count == i){
+//                     for (let j = 0;j<i;j++){
+//                         if (dir === 0){
+//                             startx -=1
+//                             console.log(startx+" row "+starty);
+//                             newstate.boxMap[startx][starty] = "X"
                             
-                        }
-                        else{
-                            starty -=1
-                            console.log(startx+" col "+starty);
-                            newstate.boxMap[startx][starty] = "X"
+//                         }
+//                         else{
+//                             starty -=1
+//                             console.log(startx+" col "+starty);
+//                             newstate.boxMap[startx][starty] = "X"
                             
-                        }
+//                         }
                         
-                    }
-                    allclear = true;
-                    if (dir === 0){
-                        startx +=1
-                    }
-                    else{
-                        starty +=1
-                    }
+//                     }
+//                     allclear = true;
+//                     if (dir === 0){
+//                         startx +=1
+//                     }
+//                     else{
+//                         starty +=1
+//                     }
                     
-                }
+//                 }
                 
 
-        }
+//         }
         
 
-        }
-        console.log("ship of "+ i)
+//         }
+//         console.log("ship of "+ i)
         
 
-    }
-    return newstate;
-}
+//     }
+//     return newstate;
+// }
 
-export default function robotReducer(state, action) {
-    if (state === undefined) {
-        console.log("running");
-        return defaultState
-    }
+// // function generateRandomAction() {
+// //     //return a random x and y
+// //     x = getRandomInt(10)
+// //     y = getRandomInt(10)
+// //     print(x, y)
+// //     return x, y
+// // }
 
-    if (action.type === 'playGame'){
-        for (var i = 0; i < state.boxMap.length; i++) {
-            for( var j = 0; j < state.boxMap[0].length; j++){
-                state.boxMap[i][j] = ''
-            }
-        }
-        return generateGameBoard()
-    }
+// export default function robotReducer(state, action) {
+//     if (state === undefined) {
+//         console.log("running");
+//         return defaultState
+//     }
 
-    if (action.type === 'robotAction') {
-        const value = state.boxMap[action.x][action.y];
-        if (value === 'X') {
-            state.boxMap[action.x][action.y] = 'B';
-            state.blackBox += 1
-        } else if (value === ''){
-            state.boxMap[action.x][action.y] = 'R';
-            state.blackBox -= 1
-        }
+
+
+//     if (action.type === 'playGame'){
+//         for (var i = 0; i < state.boxMap.length; i++) {
+//             for( var j = 0; j < state.boxMap[0].length; j++){
+//                 state.boxMap[i][j] = ''
+//             }
+//         }
+//         return generateGameBoard()
+//     }
+
+//     if (action.type === 'robotAction') {
+//         // x, y = generateRandomAction()
+//         const value = state.boxMap[action.x][action.y];
+//         if (value === 'X') {
+//             state.boxMap[action.x][action.y] = 'B';
+//             state.blackBox += 1
+//         } else if (value === ''){
+//             state.boxMap[action.x][action.y] = 'R';
+//             state.blackBox -= 1
+//         }
             
 
         
-        return { ...state };
-    }
-    if (action.type === 'resetButton') {
-        for (var i = 0; i < state.boxMap.length; i++) {
-            for( var j = 0; j < state.boxMap[0].length; j++){
-                state.boxMap[i][j] = ''
-            }
-        }
-        return generateGameBoard()
-    }
-    return state; 
-}
+//         return { ...state };
+//     }
+//     if (action.type === 'resetButton') {
+//         for (var i = 0; i < state.boxMap.length; i++) {
+//             for( var j = 0; j < state.boxMap[0].length; j++){
+//                 state.boxMap[i][j] = ''
+//             }
+//         }
+//         return generateGameBoard()
+//     }
+//     return state; 
+// }
